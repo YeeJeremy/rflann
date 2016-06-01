b5940ee01e29210af14a6765c0f0a38ad85f44c6
Jeremy Yee <jeremyyee@outlook.com.au>
2016-06-01 10:25:35 +1000

    C++ index to R indexing

--- R/RadiusSearch.R
+++ R/RadiusSearch.R
@@ -3,6 +3,8 @@
 ################################################################################
 
 RadiusSearch <- function(query, ref, radius, max_neighbour, build, cores, checks) {
-    .Call('rflann_RadiusSearch', PACKAGE = 'rflann', query, ref, radius,
-          max_neighbour, build, cores, checks)
+    output <- .Call('rflann_RadiusSearch', PACKAGE = 'rflann', query, ref,
+                    radius, max_neighbour, build, cores, checks)
+    output$indices <- lapply(output$indices, function(x){x+1})
+    return(output)
 }
